aa0e1e420b2e500656696c1f2d1e08009dff4958
luanacolombo <luuana.colombo@gmail.com>
2023-11-24 23:13:09 -0300

    Criando a Interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http; //permite que façamos as requisições
@@ -7,8 +9,18 @@ import 'dart:convert';
 Uri request = Uri.https("api.hgbrasil.com", "finance?format=json&key=aae5991f");
 
 void main() async {
-  runApp(const MaterialApp(
-    home: Home(),
+  runApp(MaterialApp(
+    home: const Home(),
+    theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: const InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          hintStyle: TextStyle(color: Colors.amber),
+        )),
   ));
 }
 
@@ -28,42 +40,91 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double? dolar;
+  double? euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: const Text("\$ Conversor \$"),
-        backgroundColor: Colors.amber,
-        centerTitle: true,
-      ),
-      body: FutureBuilder(
-          future: getData(),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-              case ConnectionState.waiting:
-                return const Center(
-                    child: Text(
-                  "Carregando Dados...",
-                  style: TextStyle(color: Colors.amber, fontSize: 25.0),
-                  textAlign: TextAlign.center,
-                ));
-              default:
-                if (!snapshot.hasError) {
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: const Text("\$ Conversor \$"),
+          backgroundColor: Colors.amber,
+          centerTitle: true,
+        ),
+        body: FutureBuilder(
+            //futuro dos dados, solicita os dados e retorna um futuro
+            future: getData(),
+            //o que vai mostrar na tela
+            builder: (context, snapshot) {
+              //ver o estado da conecção
+              switch (snapshot.connectionState) {
+                case ConnectionState.none: //se não estiver conectado
+                case ConnectionState.waiting: //ou esperando uma conexão
+                  //retorna o widget centralizado
                   return const Center(
                       child: Text(
-                    "Erro ao Carregar Dados :(",
+                    "Carregando Dados...",
                     style: TextStyle(color: Colors.amber, fontSize: 25.0),
                     textAlign: TextAlign.center,
                   ));
-                } else {
-                  return Container(
-                    color: Colors.green,
-                  );
-                }
-            }
-          }),
-    );
+                default: //caso contrário
+                  //se tem erro
+                  if (!snapshot.hasError) {
+                    return const Center(
+                        child: Text(
+                      "Erro ao Carregar Dados :(",
+                      style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                      textAlign: TextAlign.center,
+                    ));
+                    //senão
+                  } else {
+                    //valor do dolar e euro capturados do servidor
+                    dolar =
+                        snapshot.data?["results"]["currencies"]["USD"]["buy"];
+                    euro =
+                        snapshot.data?["results"]["currencies"]["EUR"]["buy"];
+                    return const SingleChildScrollView(
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Icon(Icons.monetization_on,
+                              size: 150.0, color: Colors.amber),
+                          TextField(
+                            decoration: InputDecoration(
+                                labelText: "Reais",
+                                labelStyle: TextStyle(color: Colors.amber),
+                                border: OutlineInputBorder(),
+                                prefixText: "R\$"),
+                            style:
+                                TextStyle(color: Colors.amber, fontSize: 25.0),
+                          ),
+                          Divider(),
+                          TextField(
+                            decoration: InputDecoration(
+                                labelText: "Dólares",
+                                labelStyle: TextStyle(color: Colors.amber),
+                                border: OutlineInputBorder(),
+                                prefixText: "US\$"),
+                            style:
+                                TextStyle(color: Colors.amber, fontSize: 25.0),
+                          ),
+                          Divider(),
+                          TextField(
+                            decoration: InputDecoration(
+                                labelText: "Euros",
+                                labelStyle: TextStyle(color: Colors.amber),
+                                border: OutlineInputBorder(),
+                                prefixText: "€"),
+                            style:
+                                TextStyle(color: Colors.amber, fontSize: 25.0),
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+              }
+            }));
   }
 }
